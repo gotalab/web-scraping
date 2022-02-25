bd22c7d8ca0e6a2bc3b6f3e5ca9672ade78463a0
gotaaibara <gotalab555@gmail.com>
2022-02-25 23:07:00 +0900

    Add all file and directory

---
+++ booking_project/booking/booking.py
@@ -0,0 +1,18 @@
+import os
+from typing import Any
+
+from selenium import webdriver
+
+# from booking import constants
+
+
+class Booking(webdriver.Chrome):
+    def __init__(self,
+                 driver_path: str = '/Users/gotaaibara/Documents/selenium_driver/chromedriver'
+                 ) -> None:
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Booking, self).__init__()
+
+    def land_first_page(self):
+        self.get('https://www.booking.com/')
